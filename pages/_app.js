1b5c238b9f3bd0a28a4baef92b13b910595ce323
Dend <dendysurya15@gmail.com>
2023-08-16 08:32:48 +0700

    Pembuatan Component, Service , Utils

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
-import '@/styles/globals.css'
+"use client";
+import "@/styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  <ChakraProvider>
+    return <Component {...pageProps} />
+  </ChakraProvider>;
 }
