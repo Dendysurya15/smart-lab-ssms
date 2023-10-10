ce489c6689697f788081ab5f686d713ac511ff75
jaja2302~ <valentinojaja@gmail.com>
2023-10-10 09:04:58 +0700

    export csv

--- pages/DataHistory/Upload.js
+++ pages/DataHistory/Upload.js
@@ -1,40 +1,62 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { Box, Flex, Text } from '@chakra-ui/react';
 import { useDropzone } from 'react-dropzone';
-import DashboardLayout from '../../RootLayouts/DashboardLayout/dashboardLayout'; // Import your layout
+import papaparse from 'papaparse';
+import DashboardLayout from '../../RootLayouts/DashboardLayout/dashboardLayout';
 import { Link } from '@chakra-ui/next-js';
+import DataTable from 'react-data-table-component';
 
 function Upload() {
-  const onDrop = (acceptedFiles) => {
-    // Handle the dropped files (acceptedFiles) here
-    // You can access the file details from acceptedFiles array
-    // For example, you can check the file type, size, etc.
-    console.log(acceptedFiles);
-  };
+  const [csvData, setCsvData] = useState([]);
+
+  const onDrop = useCallback((acceptedFiles) => {
+    const file = acceptedFiles[0];
+
+    papaparse.parse(file, {
+      header: true,
+      dynamicTyping: true,
+      complete: (result) => {
+        const parsedData = result.data;
+        setCsvData(parsedData);
+
+        // console.log(parsedData);
+      },
+      error: (error) => {
+        console.error('CSV parsing error:', error.message);
+      },
+    });
+  }, []);
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
     onDrop,
-    accept: '.xlsx', // Specify the accepted file types (Excel files in this case)
+    accept: '.csv',
   });
+console.log(csvData);
+  const columns = csvData.length > 0 ? Object.keys(csvData[0]).map((header) => ({
+    name: header,
+    selector: (row) => row[header],
+  })) : [];
 
   return (
     <DashboardLayout>
       <Flex direction="column" gap={4} mb={4}>
-        {/* Container for Table */}
         <Box>
-          {/* Place your table component or content here */}
-          <p>This is the table container.</p>
+          <DataTable
+            columns={columns}
+            data={csvData}
+            pagination
+            responsive
+          />
         </Box>
 
-        {/* Container for Upload File */}
         <Box {...getRootProps()} border="2px dashed" p={4} textAlign="center">
           <input {...getInputProps()} />
           {isDragActive ? (
-            <Text>Drop the Excel file here...</Text>
+            <Text>Drop the CSV file here...</Text>
           ) : (
-            <Text>Drag and drop an Excel file here, or click to select one</Text>
+            <Text>Drag and drop a CSV file here, or click to select one</Text>
           )}
-          <Text>Accepted file format: .xlsx</Text>
+          <Text>Accepted file format: .csv</Text>
         </Box>
       </Flex>
     </DashboardLayout>
